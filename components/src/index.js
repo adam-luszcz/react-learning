62a63df5aaa5473cfdb6428f5fc35879c5468331
Adam Łuszcz <adam.luszcz20@gmail.com>
2021-05-02 01:36:07 +0200

    using ApprovalCard to display comments

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:45PM"
-                commentText="Cool post!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 2:00AM"
-                commentText="Thank you for posting this!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday at 3:00PM"
-                commentText="Oh, that's bad..."
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    commentText="Cool post!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00AM"
+                    commentText="Thank you for posting this!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 3:00PM"
+                    commentText="Oh, that's bad..."
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };

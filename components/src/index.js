3adce945d58443b01f807feeb59cfe932cb354fe
Adam Łuszcz <adam.luszcz20@gmail.com>
2021-05-02 01:12:23 +0200

    added props

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                commentText="Cool post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2:00AM"
+                commentText="Thank you for posting this!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Yesterday at 3:00PM"
+                commentText="Oh, that's bad..."
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
